89be9743ded328da28d7e0e0bed81873de4e0987
Kevin COOVI <k.coovi@gmail.com>
2024-02-19 14:55:39 +0300

    main.py

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -4,9 +4,9 @@
 import requests
 
 BASE_URL = 'http://localhost:5000'
-EMAIL = "guillaume@holberton.io"
-PASSWD = "b4l0u"
-NEW_PASSWD = "t4rt1fl3tt3"
+EMAIL = "kevin.coovi.socials@gmail.com"
+PASSWD = "k1234"
+NEW_PASSWD = "npwd1234"
 
 
 def register_user(email: str, password: str) -> None:
